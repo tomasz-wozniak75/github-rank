53bc681580129d86c9a9c50f497de2fd12387449
Tomasz Wozniak <tomasz.wozniak@blue-veery.com>
2019-12-08 19:17:56 +0100

    GH_TOKEN applied

--- app/github/services/http/impl/GithubRequestExecutorImpl.scala
+++ app/github/services/http/impl/GithubRequestExecutorImpl.scala
@@ -5,17 +5,19 @@ import java.util.concurrent.TimeUnit
 import github.rank.api.{RateLimit, ToManyRequests}
 import github.services.http.api.{GithubRequestExecutor, RequestsExecutor, RequestsExecutorState}
 import javax.inject.Inject
+import play.api.Configuration
 import play.api.libs.ws.{WSClient, WSResponse}
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
-private class GithubRequestExecutorImpl @Inject()(val wsClient: WSClient) extends RequestsExecutor with GithubRequestExecutor{
+private class GithubRequestExecutorImpl @Inject()(config: Configuration, wsClient: WSClient) extends RequestsExecutor with GithubRequestExecutor{
   override val state: RequestsExecutorState = {
+    val defaultRateLimit = config.get[String]("GH_TOKEN").toInt
     val ipResponse = execute("http://checkip.amazonaws.com/")
     if (ipResponse.status == 200) {
       val ip = ipResponse.body.trim
-      new RequestsExecutorState(ip, RateLimit(60, 60, 0))
+      new RequestsExecutorState(ip, RateLimit(defaultRateLimit, defaultRateLimit, 0))
     }else {
       throw new IllegalStateException("Missing internet connection")
     }
